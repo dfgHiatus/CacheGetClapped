e255ab7ecad88cda67ade8bd3c6cb6d344c10df5
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2021-12-12 19:03:23 -0600

    more stuff

--- ModNameGoesHere/CacheGetClapped.cs
+++ ModNameGoesHere/CacheGetClapped.cs
@@ -59,11 +59,24 @@ namespace ModNameGoesHere
                     sizeDeleted += GetDirectorySize(dir);
                     dir.Delete(true);
                 }
+
+                Msg("Size of files Deleted: " + BytesToString(sizeDeleted));
             }
 
             public static long GetDirectorySize(DirectoryInfo dir)
             {
                 return dir.EnumerateFiles("*.*", SearchOption.AllDirectories).Sum(fi => fi.Length);
+            }
+
+            public static String BytesToString(long byteCount)
+            {
+                string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+                if (byteCount == 0)
+                    return "0" + suf[0];
+                long bytes = Math.Abs(byteCount);
+                int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+                double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+                return (Math.Sign(byteCount) * num).ToString() + suf[place];
             }
         }
     }
